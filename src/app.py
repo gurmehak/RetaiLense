50448d106c51071bd53c3dd179598b8980b78898
gurmehak <gurmehak99@gmail.com>
2025-03-07 22:02:25 -0800

    test header colour

--- src/app.py
+++ src/app.py
@@ -77,7 +77,16 @@ app.layout = dbc.Container([
     dcc.Store(id='selected-country-store', data=None),
     dcc.Store(id='other-countries-store', data=[]),  # Stores list of "Others" countries
 
-    dbc.Row(dbc.Col(html.H1('RetaiLense'))),
+    dbc.Row(dbc.Col(html.H1(
+        'RetaiLense',
+        style={
+            'backgroundColor': '#007bff',  
+            'color': 'white',             
+            'padding': '10px',
+            'borderRadius': '5px',
+            'textAlign': 'center', 
+            'marginBottom': '20px'
+        }))),
 
     dbc.Row([
         dbc.Col(dbc.Row([
