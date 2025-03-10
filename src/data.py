47ba83eb5c29cfe52b2132716b10d68529179157
meagangardner <meagan_gardner@hotmail.com>
2025-03-10 15:41:12 -0700

    [render preview] Parquet file (#146)
    * save filtered data to parquette file
    
    * update data.py file to read in parquet file
    
    * committed remaining pycache files

--- src/data.py
+++ src/data.py
@@ -1,10 +1,11 @@
 import pandas as pd
 
 # Read CSV file 
-df = pd.read_csv('data/processed/processed_data.csv')
+#df = pd.read_csv('data/processed/processed_data.csv')
+df = pd.read_parquet('data/processed/processed_data.parquet')
 
 # Ensure 'InvoiceDate' is converted to datetime format
 df['InvoiceDate'] = pd.to_datetime(df['InvoiceDate'])
 
 # Create Month-Year column
-df['MonthYear'] = df['InvoiceDate'].dt.strftime('%b-%Y')
+#df['MonthYear'] = df['InvoiceDate'].dt.strftime('%b-%Y')
